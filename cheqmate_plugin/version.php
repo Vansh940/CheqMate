b7f0d30871dee7632fa1065bf3a17fb0942ae8ac
Vansh940 <vanshjindam@gmail.com>
2026-07-18 21:10:03 +0530

    Inital Commit

---
+++ cheqmate_plugin/version.php
@@ -0,0 +1,8 @@
+<?php
+// Plugin version
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version = 20260305040;  // Draft state submission flow fixes and final submission rubric integration
+$plugin->requires = 2022041900;
+$plugin->component = 'assignsubmission_cheqmate';
